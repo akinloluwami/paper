f44d15b309801ef2a505ace17adef3dfb55ce4a2
akinloluwami <akinkunmioye42@gmail.com>
2024-10-07 10:44:01 +0100

    change tab title

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -17,7 +17,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: "Home",
+          title: "Explore",
           tabBarIcon: ({ color, focused }) => <Telescope color={color} />,
         }}
       />
